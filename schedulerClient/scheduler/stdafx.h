3da2fe8d3b486167ce976068b3d1f67bf59f788a
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-01-11 05:43:39 +0900

    add stdafx.h

---
+++ schedulerClient/scheduler/stdafx.h
@@ -0,0 +1,30 @@
+// stdafx.h : 標準のシステム インクルード ファイルのインクルード ファイル、または
+// 参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード ファイル
+// を記述します。
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: プログラムに必要な追加ヘッダーをここで参照してください
+/*
+winsockによるエラーの回避？
+*/
+#ifndef  _WINSOCK_DEPRECATED_NO_WARNINGS
+#define  _WINSOCK_DEPRECATED_NO_WARNINGS 1
+#endif
+
+
+/*
+TCPClient.cppにあるcopy_n()によるエラーの回避
+https://dixq.net/forum/viewtopic.php?t=17604
+*/
+#ifndef _SCL_SECURE_NO_WARNINGS
+#define _SCL_SECURE_NO_WARNINGS 1
+#endif
